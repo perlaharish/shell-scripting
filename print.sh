d8c5380d587a28b2960ac00cc708d96e4de64b37
perlaharish <perlaharish458@gmail.com>
2024-09-23 11:13:54 +0530

    Shell script print cmd

---
+++ print.sh
@@ -0,0 +1,16 @@
+echo "Hello World"
+
+#Red 31
+#Green 32
+#Yellow 33
+#Blue 34
+#magenta 35
+#cyan 36
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
+
