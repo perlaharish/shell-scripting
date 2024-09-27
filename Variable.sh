916d566c890e12967351257b7f0f2fb867826708
perlaharish <perlaharish458@gmail.com>
2024-09-27 10:24:03 +0530

    update variable script with declaration details

--- Variable.sh
+++ Variable.sh
@@ -5,9 +5,9 @@
  #$0 - $n,$#, $?
 #cmd based Substitutional variables
 
-DATE = $(date)
+DATE=$(date)
 Todaye DATE is $DATE
 #Arthemetic substitutional variables
-count= $((2+2))
+count=$((2+2))
 
 count of 2+2 is $count
